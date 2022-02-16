ca760c5b27d603badbd75983717de51dcaf5afe0
Naveen Liyanage <naveenlllkk02@gmail.com>
2022-02-16 13:45:09 +0530

    Fixed Landing Page BG issue - 2

--- vanta.js
+++ vanta.js
@@ -8,21 +8,6 @@ window.addEventListener("resize", () => {
     points = 10.0;
     maxDistance = 15.0;
   }
-
-  VANTA.NET({
-    el: "#landingPage",
-    mouseControls: true,
-    touchControls: true,
-    gyroControls: false,
-    minHeight: 100.0,
-    minWidth: 200.0,
-    scale: 1.0,
-    scaleMobile: 1.0,
-    color: "#65f0de",
-    backgroundColor: "#010f2b",
-    points: points,
-    maxDistance: maxDistance,
-  });
 });
 
 VANTA.NET({
