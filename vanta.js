9dc445ed188aa86e76ca3e573b1c5b668edf764a
Naveen Liyanage <naveenlllkk02@gmail.com>
2022-02-16 13:55:12 +0530

    Fixed Landing Page Issue

--- vanta.js
+++ vanta.js
@@ -1,26 +1,61 @@
 var width;
-var points = 15.0;
-var maxDistance = 20.0;
+// var points = 15.0;
+// var maxDistance = 20.0;
 
 window.addEventListener("resize", () => {
   width = window.innerWidth;
   if (width < 400) {
-    points = 10.0;
-    maxDistance = 15.0;
+    // points = 10.0;
+    // maxDistance = 15.0;
+
+    VANTA.NET({
+      el: "#landingPage",
+      mouseControls: true,
+      touchControls: true,
+      gyroControls: false,
+      minHeight: 100.0,
+      minWidth: 200.0,
+      scale: 1.0,
+      scaleMobile: 1.0,
+      color: "#65f0de",
+      backgroundColor: "#010f2b",
+      points: 10.0,
+      maxDistance: 15.0,
+    });
   }
 });
 
-VANTA.NET({
-  el: "#landingPage",
-  mouseControls: true,
-  touchControls: true,
-  gyroControls: false,
-  minHeight: 100.0,
-  minWidth: 200.0,
-  scale: 1.0,
-  scaleMobile: 1.0,
-  color: "#65f0de",
-  backgroundColor: "#010f2b",
-  points: points,
-  maxDistance: maxDistance,
+window.addEventListener("load", () => {
+  width = window.innerWidth;
+  if (width < 400) {
+    VANTA.NET({
+      el: "#landingPage",
+      mouseControls: true,
+      touchControls: true,
+      gyroControls: false,
+      minHeight: 100.0,
+      minWidth: 200.0,
+      scale: 1.0,
+      scaleMobile: 1.0,
+      color: "#65f0de",
+      backgroundColor: "#010f2b",
+      points: 10.0,
+      maxDistance: 15.0,
+    });
+  } else {
+    VANTA.NET({
+      el: "#landingPage",
+      mouseControls: true,
+      touchControls: true,
+      gyroControls: false,
+      minHeight: 100.0,
+      minWidth: 200.0,
+      scale: 1.0,
+      scaleMobile: 1.0,
+      color: "#65f0de",
+      backgroundColor: "#010f2b",
+      points: 15.0,
+      maxDistance: 20.0,
+    });
+  }
 });
